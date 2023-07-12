323f52f37ef6dcf0e4db0f163846426b507dea3a
amenem <aman.chourasia@gmail.com>
2023-07-12 17:20:49 +0530

    add S10 assignment

---
+++ S10/custom_resnet.py
@@ -0,0 +1,63 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CustomRes(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.prepLayer = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1, bias=False),## in_feature_map = 32, out_feature_map = 32
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.Layer1_X1 = nn.Sequential(
+            nn.Conv2d(in_channels =64, out_channels=128, kernel_size=(3,3), stride=1, padding=1, bias=False), #out_feature_map = 32
+            nn.MaxPool2d(2), #  out_feature_map = 16
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.Layer1_R1 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1, bias=False),#  out_feature_map = 16
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1, bias=False),#  out_feature_map = 16
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        self.Layer2 = nn.Sequential(
+            nn.Conv2d(in_channels =128, out_channels=256, kernel_size=(3,3), stride=1, padding=1, bias=False), ##  out_feature_map = 16
+            nn.MaxPool2d(2),##  out_feature_map = 8
+            nn.BatchNorm2d(256),
+            nn.ReLU()
+        ) 
+        self.Layer3_X2 = nn.Sequential(
+            nn.Conv2d(in_channels =256, out_channels=512, kernel_size=(3,3), stride=1, padding=1, bias=False), #out_feature_map = 8
+            nn.MaxPool2d(2), #  out_feature_map = 4
+            nn.BatchNorm2d(512),
+            nn.ReLU()
+        )
+        self.Layer3_R2 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1, bias=False),#  out_feature_map = 4
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1, bias=False),#  out_feature_map = 4
+            nn.BatchNorm2d(512),
+            nn.ReLU()
+        )   
+        self.pool = nn.MaxPool2d(4) #  out_feature_map = 1
+        self.fc = nn.Linear(512, 10)
+
+    def forward(self,X):
+        X = self.prepLayer(X)
+        X = self.Layer1_X1(X)
+        R1 = self.Layer1_R1(X)
+        X = R1+X
+        X = self.Layer2(X)
+        X = self.Layer3_X2(X)
+        R2 = self.Layer3_R2(X)
+        X = R2+X
+        X = self.pool(X)
+        X = X.view(-1,512)
+        X = self.fc(X)
+        X = F.softmax(X,dim=-1)
+        return X
+
---
+++ S10/trainer.py
@@ -0,0 +1,44 @@
+from tqdm import tqdm
+import torch
+import torch.nn.functional as F
+def train(model, optimizer, train_dataloader,device,epoch):
+    model.train()
+    pbar = tqdm(train_dataloader)
+    losses=[]
+    accs=[]
+    for idx, (data,label) in enumerate(pbar):
+        data.float().to(device)
+        label.to(device)
+        model.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, label)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss)
+        pred = output.argmax(dim=1)
+        acc = pred.eq(label).sum()/data.shape[0]
+        accs.append(acc)
+    train_avg_loss = sum(losses)/len(losses)
+    train_avg_acc = sum(accs)/len(accs)
+    print (f'at epoch:{epoch} avg_train_loss:{train_avg_loss},avg_train_acc:{train_avg_acc}')
+
+def test(model, test_dataloader,device,epoch):
+    model.eval()
+    pbar = tqdm(test_dataloader)
+    losses=[]
+    accs=[]
+    with torch.no_grad():
+        for idx, (data,label) in enumerate(pbar):
+            data.float().to(device)
+            label.to(device)
+            model.to(device)
+            output = model(data)
+            loss = F.nll_loss(output, label)
+            losses.append(loss)
+            pred = output.argmax(dim=1)
+            acc = pred.eq(label).sum()/data.shape[0]
+            accs.append(acc)
+        test_avg_loss = sum(losses)/len(losses)
+        test_avg_acc = sum(accs)/len(accs)
+        print (f'at epoch:{epoch} avg_test_loss:{test_avg_loss},avg_test_acc:{test_avg_acc}')
